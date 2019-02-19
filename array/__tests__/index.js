5b33e0d133222493d12777d7f0afb1543082dc27
raja <rbellebon@gmail.com>
2019-02-20 10:20:15 +1100

    Add forwardBackward, weeklyTemps and lettersToWord

--- array/__tests__/index.js
+++ array/__tests__/index.js
@@ -1,5 +1,23 @@
-const { createGradeObject, addGrade,  averageGrades} = require('../index');
+const { createGradeObject, addGrade,  averageGrades, forwardBackwardWords, lettersToWord} = require('../index');
 const cases = require('jest-in-case');
+const randomstring = require('randomstring')
+
+
+const generateStrings = (numberOfStrings, stringLength) => {
+  const s = new Set();
+  while (s.size < numberOfStrings) {
+    s.add(randomstring.generate(stringLength))
+  }
+
+  return s
+}
+
+const rdmStr = generateStrings(100, 20);
+const strings = [...rdmStr];
+const expectedStrings = JSON.stringify([...strings,...([...strings]).reverse()]);
+
+const letters = rdmStr.toString().split();
+const expectedLetters = rdmStr.toString();
 
 cases(
   'Create Grades',
@@ -33,5 +51,31 @@ cases(
     { name: '1 Student', grades: [78, 75, 95], expected: "82.7" },
     { name: '2 Students', grades: [78, 75, 95, 71, 50, 80, 59], expected: "72.6" },
     { name: '2+ Students', grades: [78, 75, 95, 85, 75], expected: "81.6" },
+  ],
+);
+
+cases(
+  'Words Array',
+  opts => {
+    expect(forwardBackwardWords(opts.words)).toBe(opts.expected);
+  },
+  [
+    { name: 'Empty', words: undefined, expected: undefined },
+    { name: 'Small Array', words: ["judas", "josef", "jean"], expected: JSON.stringify(["judas", "josef", "jean", "jean", "josef", "judas"])},
+    { name: 'Big Array', words: strings, expected:  expectedStrings},
+  ],
+);
+
+// TODO: Add a test for 2 arrays and display a specific week avg and all the weeks avg
+
+cases(
+  'Letters',
+  opts => {
+    expect(lettersToWord(opts.letters)).toBe(opts.expected);
+  },
+  [
+    { name: 'Empty', letters: undefined, expected: undefined },
+    { name: 'Small Array', letters: ["j", "o", "s"], expected: 'jos'},
+    { name: 'Big Array', letters: letters, expected:  expectedLetters},
   ],
 );
--- array/index.js
+++ array/index.js
@@ -7,4 +7,39 @@ const addGrade = (student/*: string */,  grades /*: object */, results /*: array
 
 const averageGrades = (studentGrades /*: Array<number>*/) => ((studentGrades.reduce((a, b) => a + b, 0))/studentGrades.length).toFixed(1);
 
-module.exports = { addGrade, averageGrades, createGradeObject };
+const forwardBackwardWords = (words/*: Array<string>*/ ) => {
+  if (words && words.length){
+    return JSON.stringify([...words, ...words.reverse()])
+  }
+}
+
+const weeklyTemps= () => {
+  this.dataStore = [];
+  this.add = add;
+  this.average = average;
+}
+
+const add = (temp1, temp2) => this.dataStore.push([temp1, temp2]);
+
+const average = () => {
+  let total = 0;
+  let avg = 0.0;
+  for (let row = 0; row , row < this.dataStore.length; ++row) {
+    for (let col = 0; col , col < this.dataStore[row].length; ++col) {
+      total += this.dataStore[row][col];
+    }
+    avg = total / this.dataStore[row].length;
+    console.log(`Average ${row+1}: ${avg}`)
+    total = 0;
+    average = 0.0;
+  }
+  return avg;
+}
+
+const lettersToWord = (letters /*: <Array<string>> */) => {
+  if (letters) {
+    return letters.join('');
+  }
+}
+
+module.exports = { addGrade, averageGrades, createGradeObject, forwardBackwardWords, lettersToWord };
